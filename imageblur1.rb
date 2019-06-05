b7843dce968775be452b7a597dfb3808803c1729
jtw1984 <watsonjamest@gmail.com>
2019-06-05 13:31:06 +0000

    Initial Commit

---
+++ imageblur1.rb
@@ -0,0 +1,30 @@
+
+
+class Image
+  attr_accessor :arr
+
+  def initialize(arr)
+    @arr = arr
+  end
+
+  def output_image
+    #Is there a difference between collect and map?
+    if @arr.empty?
+      puts "Enter valid data."
+      return
+    else
+      @arr.collect { |x| 
+      puts x.join(" ")
+    }
+  end
+  end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
